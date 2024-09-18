14cec36b6e52dc47b856b17aa9bb8319ae5c859e
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-17 21:51:45 -0300

    Comentários

---
+++ js-basico/aula02/script.js
@@ -0,0 +1,13 @@
+// Comando console - linha única
+console.log('Comentários');
+
+/*
+ * 
+ * Múltpilas linhas
+ * 
+ */
+
+console.log('Linha 1');
+console.log('Linha 2');
+// console.log('Linha 3');
+console.log('Linha 4');
