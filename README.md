8357932697aa0a659710eb89506c15c931975e49
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-17 21:27:32 -0300

    console.log

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso de JavaScript Completo
+## Aprendendo com o Otávio Miranda
---
+++ js-basico/aula03/script.js
@@ -0,0 +1,10 @@
+// Método console para mostrar textos
+
+// exibindo texto (Strings)
+console.log('Hello "World"');
+console.log("Luis 'Felipe'")
+console.log(`'Javascript' "language"`)
+
+// exibindo números (Numbers)
+console.log(1, 2, 3, 4, 5);
+console.log(1.4, 3.1415, 15.83);
