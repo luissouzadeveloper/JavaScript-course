72c1c023ff2ead13de00bd4352f314a8c06a3fb6
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-20 21:04:45 -0300

    Tipos primitivos

---
+++ js-basico/aula06/script.js
@@ -0,0 +1,22 @@
+// Tipos primitivos
+
+// String
+const nome1 = 'João';
+const nome2 = "João";
+const nome3 = `João`;
+
+// Number 
+const num1 = 30;
+const num2 = 3.14;
+
+// Undefined
+let texto;
+
+// Null
+const idade = null;
+
+// Boolean = true or false
+const aprovado = true;
+
+// Typeof = verifica o tipo
+console.log(typeof idade)
