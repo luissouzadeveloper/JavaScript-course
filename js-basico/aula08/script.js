65cfe888b4c82e1823589a9aaeda5c78b52f5bb8
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-24 21:21:13 -0300

    alert, confirm e prompt

---
+++ js-basico/aula08/script.js
@@ -0,0 +1,19 @@
+// Alerta do navegador <- Abre uma caixa de alerta
+// alert('Alerta');
+
+// // Confirm <- Abre uma caixa para 'Ok' e 'Cancelar'
+// confirm('Deseja continuar?');
+
+// // Prompt <- Abre uma caixa para inserir algo
+// prompt('Informe alguma coisa: ');
+
+// // Guardando na variável
+// const confirma = confirm('Deseja a apagar?');
+// console.log(confirma);
+
+// Exercício
+
+const n1 = Number(prompt('Informe o 1º valor: '));
+const n2 = Number(prompt('Informe o 2º valor: '));
+
+alert(`A soma entre ${n1} e ${n2} é ${n1 + n2}`);
