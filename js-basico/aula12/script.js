9da53ee06363094850dbb48088796fcf481c971d
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-26 21:07:49 -0300

    Objeto Math

---
+++ js-basico/aula12/script.js
@@ -0,0 +1,33 @@
+let num1 = 9;
+
+// floor arredonda p baixo
+let arredondaPBaixo = Math.floor(num1)
+console.log(arredondaPBaixo)
+
+// ceil arredonda p cima
+let arredondaPCima = Math.ceil(num1)
+console.log(arredondaPCima)
+
+// round arredonda p o numero inteiro mais próximo
+let arredondaPProximo = Math.round(num1);
+console.log(arredondaPProximo)
+
+// max procura o maior número em uma sequência
+console.log(Math.max(1, 2, 310, 1000, 5, 10));
+
+// min procura o menor número em uma sequência
+console.log(Math.min(1, 2, 310, 1000, 5, 10));
+
+// random gera um número aleatório entre 0 e 1 sem incluir o 1
+console.log(Math.floor(Math.random() * (10 - 1) + 1));
+
+// PI retorna o valor de PI
+console.log(Math.PI)
+
+// Pow retorna a potência
+console.log(Math.pow(num1, 2));
+
+// sqrt retorna a raiz quadrada de um número
+console.log(Math.sqrt(num1));
+
+console.log(100 / 0.0000001)
