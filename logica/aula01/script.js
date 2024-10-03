408faeea6dd167133943674a41b3a205c7f3f9c9
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-10-03 20:52:15 -0300

    Operadores de comparação

---
+++ logica/aula01/script.js
@@ -0,0 +1,25 @@
+// Operadores de comparação
+
+/*
+> maior
+>= maior ou igual
+< menor
+<= menor ou igual
+== igualdade
+=== igualdade estrita
+!= diferente
+!== diferente estrito
+*/
+
+// maior
+console.log(13 > 4);
+// maior ou igual
+console.log(13 >= 10);
+// menor que
+console.log(10 < 11);
+// menor ou igual
+console.log(30 <= 12);
+// igualdade restrita
+console.log(10 === 1); // (testa o valor e tipo!)
+// diferença estrita
+console.log(10 !== 10);
